c690e3c078eca3012dd299b654089c7e03a6b311
Harish <harish210430@gmail.com>
2025-01-09 21:05:35 -0800

    Update all

---
+++ knowingTools.js
@@ -0,0 +1,29 @@
+
+/*
+## Navigation
+CTRL + B (Open/Close Sidebar)
+CTRL + J (Open/Close Terminal)
+CTRL + Shift + F (Search)
+
+
+## Selecting
+CTRL + D (Selecting few Occurrence of Multiple Same items) // press 1 time 1st Occurence selected, press again 2nd selected, press again 3rd selected and So on.
+CTRL + Shift + L (Selecting All Occurrence of same word with just One key strok) // select one of them and just press Ctrl+Shift+L
+CTRL + Shift + up/down (MultiLine Selecting)
+CTRL + U (Undo last Multi Cursor)
+
+
+## Snippet User Configure:
+CTRL + Shift + P (Search for snippet > snippet configure) then select language for Generate a Snippet.
+Snippet: User's defined basic code structure 
+*/
+
+const num = 1;
+const num2 = 2;
+const num3 = 3;
+const num4 = 4;
+const num5 = 5;
+const num6 = 6;
+const num7 = 7;
+const num8 = 2;
+const num9 = 1;
