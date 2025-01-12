9173acd4e5b6ecff55fd4b1498ebbf6dd13291b5
Harish <harish210430@gmail.com>
2025-01-12 09:42:19 -0800

    complete the Emmet test

---
+++ Emmet_Test.js
@@ -0,0 +1,10 @@
+// Write 5 keyboard shortcuts for VSCode or any other IDE you use along with the explanation of what the shortcut does.
+/* 
+1. Shift + Alt + A  (Use to block comment)
+2. Alt + click  (Insert cursor AnyWhere)
+3. CTRL + U  (Undo last cursor Operation)
+4. CTRL + L  (Select the line)
+5. CTRL + Shift + L  (Select All the occurences of current section)
+6. CTRL + Shift + Up/Down/Left/Right  (Expand Selection to all sides)
+7. CTRL + Shift + K  (Delete the line)
+*/
