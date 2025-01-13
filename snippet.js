5b5b93b05bf85ce6874d6a2ba9b53aa64a1de8a0
Harish <harish210430@gmail.com>
2025-01-12 22:46:45 -0800

    Add snippet.js

---
+++ snippet.js
@@ -0,0 +1,22 @@
+// Trigger with : (function: two)
+
+function func_Name(arg1, arg2) {
+return arg1 + arg2;
+};
+
+// Add 3 code snippet in any language in your IDE (HTML, CSS and JS) and paste the snippet configuration below.
+
+/*
+1.
+HTML Basic Structure
+Triggered with: (html)
+
+2.
+CSS Reset
+Triggered with: (reset)
+
+3.
+Function with two Parameters:
+Triggered with: (function: two)
+
+*/
